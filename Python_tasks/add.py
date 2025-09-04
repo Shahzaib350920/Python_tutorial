81b95cf477b8fcbb70c621d28bca5cfd3f4f0a1f
Shahzaib350920 <shaheryarakhtar96@gmil.com>
2025-09-05 02:00:20 +0500

    Using two variables add 5 numbers

---
+++ Python_tasks/add.py
@@ -0,0 +1,12 @@
+num1=0
+num2=0
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num1=num1+num2
+num2=int(input("Enter third number: "))
+num1=num1+num2
+num2=int(input("Enter fourth number: "))
+num1=num1+num2
+num2=int(input("Enter fifth number: "))
+num1=num1+num2
+print(f"The total sum is {num1}")
