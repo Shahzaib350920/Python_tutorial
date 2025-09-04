933afc47f40fb404c019f7421d43423fee306a2c
Shahzaib350920 <shaheryarakhtar96@gmil.com>
2025-09-05 01:50:52 +0500

    Task solved by ubderstanding module

---
+++ Python_tasks/module.py
@@ -0,0 +1,22 @@
+num=int(input("Enter your four digit number: "))
+
+#lets suppose number is 1234
+
+
+num1=num%10    
+print(num1)
+Num1=num//10
+print(Num1)    
+num2=Num1%10   
+print(num2)
+Num2=Num1//10  
+print(Num2)
+num3=Num2%10   
+print(num3)
+Num3=Num2//10   
+print(Num3)
+
+Result=num1+num2+num3+Num3
+
+print(Result)
+
